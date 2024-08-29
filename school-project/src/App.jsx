97594d943757224054075b04317f779ada6b2bb0
Tushar Pimple <99522469+tushar782@users.noreply.github.com>
2024-08-29 22:54:48 +0530

    ERP software first commit message

---
+++ school-project/src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div>
+      <p className="text-3xl font-bold text-red-500">Scholl College Management ERP Software</p>
+    </div>
+  )
+}
+
+export default App;
